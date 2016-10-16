ab10eeead6f3458d2f00bec8c5b534a9dcfdf0c0
yunnet <yunnet@gmail.com>
2016-10-16 14:49:24 +0800

    add config file

--- src/simulate_app.erl
+++ src/simulate_app.erl
@@ -23,11 +23,12 @@ stop()->
     application:stop(simulate).
 
 start(_StartType, _StartArgs) ->
-    Host = "127.0.0.1",
-    Port = 6000,
-    CommNO = 13180000000,
+    [Host, Port] = get_server_config(simulate),
+    [Commno, Num] = get_client_config(simulate),
+
     {ok,SupPid} = simulate_sup:start_link(),
-    simulate:start(10, Host, Port, CommNO),
+
+    simulate:start(Num, Host, Port, Commno),
     {ok, SupPid}.
 
 %%--------------------------------------------------------------------
@@ -37,3 +38,26 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+get_server_config(App)->
+    case application:get_env(App, server) of
+        {ok, false} -> throw(undefined);
+        {ok, Server_config} ->
+            try
+                {_, Host} = lists:keyfind(host, 1, Server_config),
+                {_, Port} = lists:keyfind(port, 1, Server_config),
+                [Host, Port]
+            catch
+                _:_ -> exit({bad_config, {simulate, {server, config_error}}})
+            end;
+        undefined -> throw(undefined)
+    end.
+
+get_client_config(App)->
+    case application:get_env(App, client) of
+        {ok, false} -> throw(undefined);
+        {ok, Client_config} ->
+            {_, Commno} = lists:keyfind(commno, 1, Client_config),
+            {_, Num} = lists:keyfind(num, 1, Client_config),
+            [Commno, Num];
+        undefined -> throw(undefined)
+    end.
